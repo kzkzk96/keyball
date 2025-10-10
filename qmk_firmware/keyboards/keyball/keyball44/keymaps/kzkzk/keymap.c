cc420260865f1c486c5079d57a9d4642bed5d0e7
kzkzk96 <zodia9ue@gmail.com>
2025-10-10 23:21:58 +0900

    Update keymap.c

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/kzkzk/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/kzkzk/keymap.c
@@ -181,7 +181,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // レイヤ処理 / OLED表示
 // ============================================================
 layer_state_t layer_state_set_user(layer_state_t state) {
-    keyball_set_scroll_mode(get_highest_layer(state) == 5);
+    // 自動スクロールの無効化
+    // keyball_set_scroll_mode(get_highest_layer(state) == 5);
     return state;
 }
 
